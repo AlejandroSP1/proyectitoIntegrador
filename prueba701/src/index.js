badd2696ede50c86f778cef38ed1c20cf94da4b0
Pablo <alexsegundo185@gmail.com>
2024-11-15 11:31:47 -0600

    cambioCal

--- prueba701/src/index.js
+++ prueba701/src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ListaDocentes from './Trabajadores/Docente';
+import Calificaciones from './Alumnos/Calificaciones';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ListaDocentes/>
+    <Calificaciones/>
   </React.StrictMode>
 );
 
